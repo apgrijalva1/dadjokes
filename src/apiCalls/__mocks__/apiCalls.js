a391094cf55416619ef7c49adfd04fed222e4b17
Kelly J Andrews <kelly@kellyjandrews.com>
2018-06-13 00:56:06 -0400

    cleanup, stitch webhook usage

--- src/apiCalls/__mocks__/apiCalls.js
+++
@@ -1,9 +0,0 @@
-export const fetchJoke = jest.fn()
-  .mockImplementationOnce(() => ({
-    "id": "R7UfaahVfFd",
-    "joke": "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
-    "status": 200
-  }))
-  .mockImplementationOnce(() => {
-    throw(new Error('Error fetching joke'))
-  })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App.jsx';
-// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const baseUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/<APP NAME>/service/<SERVICE NAME>/incoming_webhook/<WEBHOOK>?secret=<SECRET>';
+const opts = {
+  headers: {
+    'Accept': 'application/json'
+  }
+}
+
+function getRandomColor() {
+
+  let hue = Math.floor(Math.random() * 255);
+  const styles = {
+    backgroundColor: `hsl(${hue}, 60%, 20%)`,
+    color: `hsl(${hue}, 60%, 80%)`
+  }
+  return styles;
+}
+
+const App = ({joke, styles}) => {
+  return(
+    <div className='joke-wrapper' style={styles} >
+      {joke}
+    </div>
+  )
+};
+
+async function startApp() {
+  const response = await fetch(baseUrl, opts);
+  const data = await response.json();
+  const styles = getRandomColor();
+  ReactDOM.render(
+    <App
+      joke={data.joke}
+      styles={styles}
+    />,
+  document.getElementById('root'));
+}
+
+startApp();
